a57b59b81c60523f49fe70dc9f580319451038cb
Paridhi-28 <159625617+Paridhi-28@users.noreply.github.com>
2024-06-03 20:04:17 +0530

    Add files via upload

---
+++ tic-tac-toe.js
@@ -0,0 +1,4 @@
+const playButton =document.getElementByClassName('play')[0];
+const lapButton =document.getElementByClassName('lap')[0];
+const resetButton =document.getElementByClassName('reset')[0];
+
